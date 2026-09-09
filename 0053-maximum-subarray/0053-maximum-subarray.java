8ead86406d95cd368e58bcedf8776ec518f8c2ce
tinu2962-sys <tinu2962@gmail.com>
2026-09-09 23:34:35 +0530

    Time: 1 ms (99.93%), Space: 77.1 MB (82.73%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int cs = nums[0];
+        int ms = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            cs = Math.max(nums[i], cs + nums[i]);
+
+            ms = Math.max(ms, cs);
+        }
+
+        return ms;
+    }
+}
